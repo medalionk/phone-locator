48ec23e6ff4818fa15f7a535248c37598c77db4e
medalionk <abdullahbilal88@gmail.com>
2017-08-28 20:09:58 +0300

    Added FloatingActionButton to recorder fragment

--- app/src/main/java/apps/jibovico/dev/phonelocator/fragments/RecorderFragment.java
+++ app/src/main/java/apps/jibovico/dev/phonelocator/fragments/RecorderFragment.java
@@ -2,6 +2,8 @@ package apps.jibovico.dev.phonelocator.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -39,8 +41,19 @@ public class RecorderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recorder, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_recorder, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        return view;
     }
 
 }
